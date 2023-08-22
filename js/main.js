9be1ec188a0a881d9d3b34e939763f5442ae0638
Matias Peña <pena_matias@hotmail.com>
2023-08-22 09:52:56 -0300

    Implemented only js prompt and alert for first deploy

--- js/main.js
+++ js/main.js
@@ -1,24 +1,35 @@
-function loan(){
-  let amount = document.getElementById("loans-article-amount");
+// El codigo comentado sera implementado en la version final del proyecto.
 
-  let amountValue = parseInt(prompt("Ingrese el monto del prestamo:\n$500000(500000)\n$1000000(1000000)"));
-  
+function loan() {
+  // let amount = document.getElementById("loans-article-amount");
+
+  let amountValue = parseInt(
+    prompt(
+      "Bienvenido a su prestamo de Banco Libertad\n\
+Ingrese el monto del prestamo:\n$500000(500000)\n$1000000(1000000)"
+    )
+  );
+
   while (amountValue != 500000 && amountValue != 1000000) {
     alert("Ingrese un monto valido");
-    amountValue = parseInt(prompt("Ingrese el monto del prestamo:\n$500000(500000)\n$1000000(1000000)"));
+    amountValue = parseInt(
+      prompt(
+        "Ingrese el monto del prestamo:\n$500000(500000)\n$1000000(1000000)"
+      )
+    );
   }
-  amount.innerHTML = "Monto: $" + amountValue;
+  // amount.innerHTML = "Monto: $" + amountValue;
 
-  let months = document.getElementById("loans-article-months");
+  // let months = document.getElementById("loans-article-months");
 
   let monthsValue = parseInt(prompt("Ingrese el numero de meses:\n6\n12"));
 
   while (monthsValue != 6 && monthsValue != 12) {
-    alert("Ingrese un monto valido");
+    alert("Ingrese una cantidad de meses valida");
     monthsValue = parseInt(prompt("Ingrese el numero de meses:\n6\n12"));
   }
 
-  months.innerHTML = "Meses: " + monthsValue;
+  // months.innerHTML = "Meses: " + monthsValue;
 
   let taxValue;
 
@@ -28,24 +39,38 @@ function loan(){
     taxValue = 1.5;
   }
 
-  let tax = document.createElement("p");
-  tax.innerHTML = 'Tasa del: ' + taxValue * 100 + '%';
+  let tax = "Tasa del: " + taxValue * 100 + "%";
 
-  let p = document.getElementById("loans-article-total");
+  // let p = document.getElementById("loans-article-total");
 
-  p.innerHTML = "";
+  // p.innerHTML = "";
 
-  let total = (amountValue * taxValue);
+  let total = amountValue * taxValue;
 
   let quota = total / monthsValue;
 
-  let articleTotal = document.createElement("p");
-  articleTotal.innerHTML = 'Total a pagar: $' + total;
+  let articleTotal = "Total a pagar: $" + total;
 
-  let articleQuota = document.createElement("p");
-  articleQuota.innerHTML = 'Valor de la cuota: $' + quota;
+  let articleQuotaL = "Valor de la cuota: $" + quota;
 
-  p.appendChild(tax);
-  p.appendChild(articleTotal);
-  p.appendChild(articleQuota);
+  // p.appendChild(tax);
+  // p.appendChild(articleTotal);
+  // p.appendChild(articleQuota);
+
+  alert(
+    "Prestamo solicitado con exito!\n" +
+      "Monto: $" +
+      amountValue +
+      "\n" +
+      tax +
+      "\n" +
+      articleTotal +
+      "\n" +
+      "Cantidad de cuotas: " +
+      monthsValue +
+      "\n" +
+      articleQuotaL
+  );
 }
+
+loan();
