810e2d65fbdcd1bd1934d3fe66bb3ca0125b8c85
Matias Peña <pena_matias@hotmail.com>
2023-09-03 10:18:58 -0300

    Added some styles, start implementing of investments section in JS

--- js/main.js
+++ js/main.js
@@ -1,4 +1,17 @@
 // El codigo comentado sera implementado en la version final del proyecto.
+
+function bienvenido() {
+  let opcion = parseInt(prompt("BIENVENIDO A SU BANCO LIBERTAD\nIngrese su opcion:\n1.Inversiones\n2.Prestamos"));
+  while (opcion != 1 && opcion != 2) {
+    alert("Ingrese una opcion valida");
+    opcion = parseInt(prompt("BIENVENIDO A SU BANCO LIBERTAD\nIngrese su opcion:\n1.Inversiones\n2.Prestamos"));
+  }
+  if (opcion == 1) {
+    investments();
+  } else if (opcion == 2) {
+    loan();
+  }
+}
 function loan() {
   // let amount = document.getElementById("loans-article-amount");
 
@@ -17,9 +30,9 @@ Ingrese el monto del prestamo:\n$500000(500000)\n$1000000(1000000)"
       )
     );
   }
-  // amount.innerHTML = "Monto: $" + amountValue;
+  amount.innerHTML = "Monto: $" + amountValue;
 
-  // let months = document.getElementById("loans-article-months");
+  let months = document.getElementById("loans-article-months");
 
   let monthsValue = parseInt(prompt("Ingrese el numero de meses:\n6\n12"));
 
@@ -28,7 +41,7 @@ Ingrese el monto del prestamo:\n$500000(500000)\n$1000000(1000000)"
     monthsValue = parseInt(prompt("Ingrese el numero de meses:\n6\n12"));
   }
 
-  // months.innerHTML = "Meses: " + monthsValue;
+  months.innerHTML = "Meses: " + monthsValue;
 
   let taxValue;
 
@@ -72,4 +85,49 @@ Ingrese el monto del prestamo:\n$500000(500000)\n$1000000(1000000)"
   );
 }
 
-loan();
+let portfolio = {
+  acciones: [
+    {
+      nombre: "AR123",
+      precio: 1234,
+      cantidad: 1,
+      },
+    {
+      nombre: "AR1235",
+      precio: 1234,
+      cantidad: 1,
+    }
+  ],
+  bonos: [
+    {
+      nombre: "Bonar123",
+      precio: 1234,
+      cantidad: 1,
+    },
+    {
+      nombre: "Bonar1235",
+      precio: 1234,
+      cantidad: 1,
+    }
+  ]
+}
+function investments(){
+  let opciones = [];
+  let contador = 1;
+  for (element in portfolio){
+    opciones.push(`${contador})${element.toUpperCase()}`);
+    contador++;
+  };
+  console.log(opciones);
+  let opcion = parseInt(prompt(`Elija el tipo de inversion:\n${opciones}`));
+  while (opcion != 1 && opcion != 2) {
+    alert("Ingrese una opcion valida");
+    opcion = parseInt(prompt(`Elija el tipo de inversion:\n${opciones}`));
+  }
+  if (opcion == 1) {
+    
+  }
+}
+
+// bienvenido();
+investments();
